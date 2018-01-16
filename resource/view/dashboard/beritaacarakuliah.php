7c8ff2bf801517d27fc25df7fa9bf92510e748ba
saadilahmustari <saadilahmustari99@gmail.com>
2018-01-16 13:34:07 +0700

    Absen Dosen

---
+++ resource/view/dashboard/beritaacarakuliah.php
@@ -0,0 +1,303 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Berita Acara Kuliah</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="../../../public/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../../public/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../../../public/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="../../../public/admin/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../../../public/admin/dist/css/skins/skin-blue.min.css">
+  <!-- Google Font -->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>A</b>LT</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>SIA</b>kad</span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">          
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="../../../public/admin/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">Mukhlis Febriansyah</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="../../../public/admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Mukhlis Febriansyah - Web Developer of SMK WIKRAMA BOGOR
+                  <small>Member since Nov. 2012</small>
+                </p>
+              </li>
+              <!-- Menu Body -->
+              <li class="user-body">
+                <div class="row">
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Followers</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Sales</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Friends</a>
+                  </div>
+                </div>
+                <!-- /.row -->
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="../login/login-mahasiswa.php" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../../../public/admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Mukhlis Febriansyah</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">HEADER</li>
+        <!-- Optionally, you can add icons to the links -->                
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Kartu Rencana Study</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Kartu Rencana Study</a></li>
+            <li><a href="#">Kartu Hasil Study</a></li>
+            <li><a href="#">Transkrip Nilai</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Kartu Hasil Study</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Kartu Rencana Study</a></li>
+            <li><a href="#">Kartu Hasil Study</a></li>
+            <li><a href="#">Transkrip Nilai</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Transkrip Nilai</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Kartu Rencana Study</a></li>
+            <li><a href="#">Kartu Hasil Study</a></li>
+            <li><a href="#">Transkrip Nilai</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Input Data</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="daftar/daftarmahasiswa.php">Pendaftaran Mahasiswa</a></li>
+          </ul>
+        </li>
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Input Data Mata Kuliah
+      </h1>    
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">      
+        <div class="box box-default">
+      <div class="box-header with-border">
+        <h3 class="box-title">Form Input Data Berita Acara Kuliah</h3>
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse">
+              <i class="fa fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove">
+              <i class="fa fa-remove"></i>
+            </button>
+          </div>
+          <div class="box-body">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Program Study ID</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-gg-circle"></i>
+                    </div>
+                    <input type="text" class="form-control pull-right" required="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>ID Dosen</label>
+                  <select class="form-control select2 select2-hidden-accessible pull-right" style="width:100%;" tabindex="-1" aria-hidden="true" required="">
+                    <option selected="selected" disabled="disabled">Nama Dosen</option>
+                    <option>1</option>
+                    <option>2</option>
+                  </select>
+                </div>
+              </div>              
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Tanggal</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-building"></i>
+                    </div>
+                    <input type="date" class="form-control" required="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Acara Mulai/Masuk</label>
+                  <div class="input-group pull-right">
+                    <div class="input-group-addon">
+                      <i class="fa fa-hourglass-half"></i>
+                    </div>
+                    <input type="time" class="form-control" data-inputmask=""mask": "(999) 999-9999"" data-mask required="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Acara Selesei</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-windows"></i>
+                    </div>
+                    <input type="time" class="form-control" data-inputmask=""mask": "(999) 999-9999"" data-mask required="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Materi yang di sampaikan</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-windows"></i>
+                    </div>
+                    <input type="text" class="form-control" data-inputmask=""mask": "(999) 999-9999"" data-mask required="">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-grup">
+                  <label>Jumlah Mahasiswa</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-windows"></i>
+                    </div>
+                    <input type="number" class="form-control" data-inputmask=""mask": "(999) 999-9999"" data-mask required="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>    
+      </div>
+    </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
+  
+  <!-- Add the sidebar's background. This div must be placed
+  immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 3 -->
+<script src="../../../public/admin/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../../public/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../../public/admin/dist/js/adminlte.min.js"></script>
+
+</body>
+</html>
